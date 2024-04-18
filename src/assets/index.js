a2c646682b37a57c3dfd4ed3f1b5f0cac1163941
utsav-07 <utsavudit07@gmail.com>
2024-04-18 19:24:45 +0530

    added api

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import m_icon from "./m_icon.png";
+import maximize from "./maximize.png";
+import micon from './micon.png'
+
+
+
+export {m_icon , maximize,micon};
--- src/component/Videoframe.js
+++ src/component/Videoframe.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './Videoframe.css';
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
-
+import { m_icon } from '../assets';
 const Videoframe = () => {
     const [screen1, setScreen1] = useState(false);
     const [screen2, setScreen2] = useState(false);
@@ -20,16 +20,19 @@ const Videoframe = () => {
 
     const onOpenModalScrren4 = () => setScreen4(true);
     const onCloseModalScrren4 = () => setScreen4(false);
-
     
-    const video1 = "https://www.youtube.com/embed/z9eWUVZKPE8?autoplay=1";
-    const websiteLink = "https://react.dev/";
+    
+    const video1 = "https://www.youtube.com/embed/smpE8K2ylPw?autoplay=1";
+    const proxyUrl = "https://grid-server-ewal.onrender.com/api/proxy-website";
+
+    const websiteLink = proxyUrl;
 
     return (
         <div className="parent">
             <div className="video-container">
                 <iframe width="100%" height="100%" src={websiteLink} title="Website 1" frameBorder="0" allowFullScreen></iframe>
-                <button onClick={onOpenModalScrren1}>Open</button>
+                {/* <button onClick={onOpenModalScrren1}>[]</button> */}
+                <img alt="logo" onClick={onOpenModalScrren1} src={m_icon} style={{height : '30px' ,  width : '30px'}} />
                 <Modal
                     styles={{
                         modal: {
@@ -46,7 +49,7 @@ const Videoframe = () => {
             </div>
             <div className="video-container">
                 <iframe width="100%" height="100%" src={websiteLink} title="Website 2" frameBorder="0" allowFullScreen></iframe>
-                <button onClick={onOpenModalScrren2}>Open</button>
+                <img alt="logo" onClick={onOpenModalScrren2} src={m_icon} style={{height : '30px' ,  width : '30px'}} />
                 <Modal
                     styles={{
                         modal: {
@@ -63,7 +66,7 @@ const Videoframe = () => {
             </div>
             <div className="video-container">
                 <iframe width="100%" height="100%" src={video1} title="Video 3" frameBorder="0" allowFullScreen></iframe>
-                <button onClick={onOpenModalScrren3}>Open</button>
+                <img alt="logo" onClick={onOpenModalScrren3} src={m_icon} style={{height : '30px' ,  width : '30px'}} />
                 <Modal
                     styles={{
                         modal: {
@@ -79,8 +82,8 @@ const Videoframe = () => {
                 </Modal>
             </div>
             <div className="video-container">
-                <iframe width="100%" height="100%" src={video1} title="Video 4" frameBorder="0" allowFullScreen></iframe>
-                <button onClick={onOpenModalScrren4}>Open</button>
+                <iframe width="100%" height="100%" src={video1} title="Video 4" frameBorder="0" allowFullScreen></iframe> 
+                <img alt="logo" onClick={onOpenModalScrren4} src={m_icon} style={{height : '30px' ,  width : '30px'}} />
                 <Modal
                     styles={{
                         modal: {
